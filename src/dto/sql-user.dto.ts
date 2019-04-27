346548e1ff51ff52b7c0f3a01237456574eb8a45
Ric Davis <rickjay0@gmail.com>
2019-04-27 02:07:24 -0400

    Fixed everything hopefully

---
+++ src/dto/sql-user.dto.ts
@@ -0,0 +1,11 @@
+
+export class SqlUser {
+    user_id = 0;
+    username = '';
+    password = '';
+    first_name = '';
+    last_name = '';
+    email = '';
+    role = undefined;
+    img = '';
+}
---
+++ src/index.ts
@@ -0,0 +1,56 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { userRouter } from './routers/userRouter';
+import { reimbursementRouter } from './routers/reimburseRouter';
+import { sessionMiddleware } from './middleware/session.middleware';
+import * as userDao from './daos/users.dao';
+
+const app = express();
+const port = process.env.SHIP_PORT || 8080;
+
+app.use(bodyParser.json());
+
+app.use(sessionMiddleware);
+
+// allow cross origins
+app.use((req, resp, next) => {
+    console.log(req.get('host'));
+    (process.env.SHIP_API_STAGE === 'prod')
+      ? resp.header('Access-Control-Allow-Origin', process.env.SHIP_APP_URL)
+      : resp.header('Access-Control-Allow-Origin', `${req.headers.origin}`);
+    resp.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    resp.header('Access-Control-Allow-Credentials', 'true');
+    resp.header('Access-Control-Allow-Methods', 'POST, GET, DELETE, PUT, PATCH');
+    next();
+});
+
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    const user = await userDao.findUsernameAndPassword(username, password);
+
+    if (user) {
+        req.session.user = user;
+        console.log('User successfully signed in...');
+        res.json(user);
+    } else {
+        const resp = {
+            message: 'Invalid credentials'
+        };
+        res.status(400).json(resp);
+    }
+});
+
+app.use('/hi', (req, res) => {
+    res.send('Hi I work.');
+});
+
+/**
+ * Register routers
+ */
+app.use('/users', userRouter);
+app.use('/reimbursements', reimbursementRouter);
+
+app.listen(port, () => {
+    console.log('application started');
+});
+
